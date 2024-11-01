784e33c761461df557fca10dcaca7ad1ee8d2914
jgranger3 <jgranger3@ivytech.edu>
2024-11-01 18:36:10 -0400

    Made the TODO task, for them to show.

--- todoList/App.js
+++ todoList/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <view style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <view style={styles.items}>
+          <Task text={'Task 1'}/>
+          <Task text={'Task 2'}/>
+        </view>
+      </view>
     </View>
   );
 }
@@ -13,8 +19,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+
 });

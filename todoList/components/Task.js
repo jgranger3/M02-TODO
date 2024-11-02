d7ef43eeb2850936d32034a774e97b80cdfa9fbf
jgranger3 <jgranger3@ivytech.edu>
2024-11-02 00:21:53 -0400

    updated the props

--- todoList/components/Task.js
+++ todoList/components/Task.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const Task = () => {
+const Task = (props) => {
 
     return (
         <View style={styles.item}>
